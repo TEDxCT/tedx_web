9e136c9205103f53471e6b82cac8c9eb9ab12a17
Craig Wattrus <cwattrus@gmail.com>
2014-12-24 19:25:45 +0200

    Adding a home page. Changed some of the colors

--- server/publications.js
+++ server/publications.js
@@ -2,10 +2,14 @@ Meteor.publish('videos', function () {
   return videos.find();
 });
 
+Meteor.publish('topvideos', function () {
+  return videos.find();
+});
+
 Meteor.publish('favorites', function () {
   // console.log(videos.find({"owner":this.userId, "archived": { $ne: true } }))
   // return videos.find({"owner":this.userId, "archived": { $ne: true } });
   if(this.userId) {
-    return favorites.find({"owner":this.userId, "archived": null});  
+    return favorites.find({"owner":this.userId, "archived": null});
   }
 });
