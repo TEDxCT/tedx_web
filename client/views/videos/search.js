b21e803c923f668007f39b74fe5734086c608d07
Craig Wattrus <cwattrus@gmail.com>
2014-12-25 22:45:47 +0200

    More searchy stuff

--- client/views/videos/search.js
+++ client/views/videos/search.js
@@ -1,27 +1,29 @@
 Template.search.events({
-    'click .close' : function() {
+    'click .search-close.close' : function() {
         Session.set("searching", false);
+        Session.set("search-term", "");
     },
     'keyup #search_text': function(event, template) {
         Session.set("search-term", template.find('#search_text').value);
-
     },
 });
 
 Template.search.helpers({
   "videos" : function() {
     var searchString = Session.get("search-term");
-    console.log("Searc");
     if(searchString) {
       var someCursor = videos.find({ name: searchString });
-console.log("Searc");
       if(someCursor.count() == 0)
       {
           var search = ".*" + searchString + ".*";
-          console.log("Searc");
           return videos.find({"name" : {$regex : search}});
       }
     }
     else return "";
   },
 });
+
+
+Template.search.rendered = function() {
+    $("#search_text").focus();
+}
