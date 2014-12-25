667d8dba31fbe55d244b55813241b1608b487454
Craig Wattrus <cwattrus@gmail.com>
2014-12-25 21:54:44 +0200

    Adding masonry stuff in preparation for more epic video grid. Alos started work on the talks page and tabs.

--- client/front.js
+++ client/front.js
@@ -10,8 +10,18 @@ Template.toolbar.events({
   },
   'keyup #search_text': function(event, template) {
     Session.set("search-term", template.find('#search_text').value);
-  }
+  },
 });
+
+Template.toolbar.helpers({
+  activeIfTemplateIs: function (template) {
+      var currentRoute = Router.current();
+      console.log(currentRoute.lookupTemplate());
+      console.log(template);
+      return currentRoute &&
+        template.toLowerCase() === currentRoute.lookupTemplate().toLowerCase() ? 'active' : '';
+    }
+})
 
 Meteor.subscribe('favorites', function onReady() {
   Session.set('favoritesLoaded', true);
