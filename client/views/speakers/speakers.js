3d8da1ea529c18869d5b33404b7bae6d5174937b
Carla Goldstein <carla.goldstein@gmail.com>
2015-02-07 17:34:08 +0200

    Added thank you message on completion. Can update once we have more content.

--- client/views/speakers/speakers.js
+++ client/views/speakers/speakers.js
@@ -2,4 +2,18 @@ Template.register.events({
 	'click .cta' : function() {
 		Router.go('speakers.register.apply');
 	},
+
+});
+
+Template.nomination.events({
+	'submit' : function() {
+		Router.go('speakers.register.complete');
+	}
+});
+
+
+Template.application.events({
+	'submit' : function() {
+		Router.go('speakers.register.complete');
+	}
 });
