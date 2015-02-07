7ece597ba43712765d6605d3476dee490c7a434d
Carla Goldstein <carla.goldstein@gmail.com>
2015-02-07 16:37:39 +0200

    updated speaker schema

--- models/speaker.js
+++ models/speaker.js
@@ -1,11 +1,94 @@
 SpeakerApplicationSchema = new SimpleSchema({
+preferredName: {
+    type: String,
+    label: "Preferred Name",
+    optional: false,
+  },
 email: {
     type: String,
     label: "Email Address",
     optional: false
-
   },
+cellphone: {
+  type: Number,
+  label: "Cellphone Number",
+  optional: true
+},
+TEDProfileURL: {
+  type: String,
+  label: "URL to your TED.com profile",
+  optional: false,
+},
+TEDTalks: {
+  type: String,
+  label: "The TED talk/s that designed your thinking",
+  optional: false,
+},
+moreInfo: {
+  type: String,
+  label: "Where can we find out more about you?",
+  optional: true,
+},
+ideaWorthSpreading: {
+  type: String,
+  label: "What is your idea worth spreading?",
+  optional: false,
+},
+gender: {
+    type: String,
+    optional: true,
+    label: "Gender",
+    autoform: {
+      type: "select",
+      options: function () {
+        return [
+          {label: "Female", value: "female"},
+          {label: "Male", value: "male"},
+          {label: "Other", value: "other"}
+        ];
+      }
+    }
+  },
+  ageGroup: {
+    type: String,
+    optional: true,
+    label: "Age Group",
+    autoform: {
+      type: "select",
+      options: function () {
+        return [
+          {label: "Younger than 18", value: "less than 18"},
+          {label: "18-25", value: "18-25"},
+          {label: "26-35", value: "26-35"},
+          {label: "36-45", value: "36-45"},
+          {label: "46-55", value: "46-55"},
+          {label: "55-64", value: "55-64"},
+          {label: "65+", value: "more than 65"}
+        ];
+      }
+    }
+  },
+  race: {
+  type: String,
+  optional: true,
+  label: "Race",
+  autoform: {
+    type: "select",
+    options: function () {
+      return [
+        {label: "Prefer not to say", value: "Prefer not to say"},
+        {label: "Black", value: "Black"},
+        {label: "Coloured", value: "Coloured"},
+        {label: "Indian/Asian", value: "Indian/Asian"},
+        {label: "White", value: "White"},
+        {label: "Other", value: "Other"},
+      ];
+    }
+  }
+}
 });
+
+
 
 SpeakerNominationSchema = new SimpleSchema({
 nominatorFirstName: {
