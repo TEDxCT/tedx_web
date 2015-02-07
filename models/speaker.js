45d6a83ed6cf0272de82008ef179fb478477b3d7
Carla Goldstein <carla.goldstein@gmail.com>
2015-02-07 16:08:50 +0200

    added speaker model properties for nominations.

--- models/speaker.js
+++ models/speaker.js
@@ -1,24 +1,62 @@
-ContactDetailsSchema = new SimpleSchema({
-  name: {
+SpeakerApplicationSchema = new SimpleSchema({
+email: {
     type: String,
-    label: "Contact Name",
-  },
-  phoneNumber: {
-    type: Number,
-    label: "Contact Number",
-  },
-  email: {
-    type: String,
-    label: "Contact Email"
+    label: "Email Address",
+    optional: false
+
   },
 });
+
+SpeakerNominationSchema = new SimpleSchema({
+nominatorFirstName: {
+    type: String,
+    label: "Nominator First Name",
+    optional: false
+  },
+  nominatorLastName: {
+    type: String,
+    label: "Nominator Last Name",
+    optional: false
+  },
+  nominatorEmail: {
+    type: String,
+    label: "Nominator Email Address",
+    optional: false
+  },
+  speakerContact: {
+    type: String, 
+    label: "Please provide contact details/information for the speaker you want to nominate. This could be an e-mail address, a cellphone number, a website, or a social media account",
+optional: false
+  },
+  speakerIdea: {
+    type: String,
+    label: "What is this speakers' idea? and why is it worth spreading?",
+    optional: false,
+  }
+});
+
 
 SpeakerSchema = new SimpleSchema({
-contact: {
-    type: ContactDetailsSchema,
-    optional: true
-
+  firstName: {
+    type: String,
+    label: "First Name",
+    optional: false
   },
+  lastName: {
+    type: String,
+    label: "Last Name",
+    optional: false
+  },
+  speakerApplication: {
+    type: SpeakerApplicationSchema,
+    label: "Speaker Application",
+  },
+  speakerNomination: {
+    type: SpeakerNominationSchema,
+    label: "Speaker Nomination",
+  }
 });
+
+
 
 speakers.attachSchema(SpeakerSchema);
