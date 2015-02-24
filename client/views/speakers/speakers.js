9c58ac73489253599e7dbda604238764026e355b
Craig Wattrus <cwattrus@gmail.com>
2015-02-24 18:42:09 +0200

    Removing some spacing and the is client if statement since this is in the client folder it is unecessary

--- client/views/speakers/speakers.js
+++ client/views/speakers/speakers.js
@@ -1,39 +1,33 @@
-if (Meteor.isClient) {
-	Template.register.events({
-		'click .cta' : function() {
-			Router.go('speakers.register.apply');
+Template.register.events({
+	'click .cta' : function() {
+		Router.go('speakers.register.apply');
+	},
+
+});
+
+SimpleSchema.debug = true;
+
+AutoForm.hooks({
+  insertSpeakerApplicationForm: {
+    	onSuccess: function(operation, result, template) {
+			Router.go('speakers.register.complete');
 		},
-
-	});
-
-
-
-	SimpleSchema.debug = true;
+	}
+});
 
 
+AutoForm.hooks({
+  insertSpeakerNominationForm: {
+    	onSuccess: function(operation, result, template) {
+			Router.go('speakers.register.complete');
+		},
+	}
+});
 
-	AutoForm.hooks({
-	  insertSpeakerApplicationForm: {
-	    	onSuccess: function(operation, result, template) {	
-				Router.go('speakers.register.complete');
-			},
-		}
-	});
+Handlebars.registerHelper('isEqual', function(string1, string2) {
+		return string1 === string2;
+});
 
-
-	AutoForm.hooks({
-	  insertSpeakerNominationForm: {
-	    	onSuccess: function(operation, result, template) {
-				Router.go('speakers.register.complete');
-			},
-		}
-	});
-
-	Handlebars.registerHelper('isEqual', function(string1, string2) {
-			return string1 === string2;
-	});
-
-	Handlebars.registerHelper('setSpeakerRegistrationType', function (type) {
-	      Session.set("speakerRegistrationType", type);
-	});
-}
+Handlebars.registerHelper('setSpeakerRegistrationType', function (type) {
+      Session.set("speakerRegistrationType", type);
+});
