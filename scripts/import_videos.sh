a225b3dc89f6a0a2cecc0b3da6bc1eaf4974cd8a
Carla Goldstein <carla.goldstein@gmail.com>
2015-06-02 10:23:17 +0200

    json file for videos now correct. Removed escaped single quotes \' -> '

--- scripts/import_videos.sh
+++ scripts/import_videos.sh
@@ -38,15 +38,20 @@ fi;
 # mysql -u [user] -p[pass] -e "[mysql commands]"
 # mysql -h "https://arya.texo.co.za:2083" -u "carla" "333" "tedxcape_drup" < "videos.sql"
 
-# perl -pi -e 's/field_video_description/description/g' videos.json
-# perl -pi -e 's/title/name/title/g' videos.json
-# perl -pi -e 's/field_video_embed/url/g' videos.json
-# perl -pi -e 's/field_video_provider/provider/g' videos.json
-# perl -pi -e 's/field_video_data/data/g' videos.json
-# perl -pi -e 's/field_video_duration/duration/g' videos.json
-# perl -pi -e 's/field_video_value/videoId/g' videos.json
-# perl -pi -e 's/\"{/{/g' videos.json
-perl -pi -e 's/"published": 1/"published": true/g' videos.json
+# perl -pi -e "s/field_video_description/description/g" videos.json
+# perl -pi -e "s/title/name/title/g" videos.json
+# perl -pi -e "s/field_video_embed/url/g" videos.json
+# perl -pi -e "s/field_video_provider/provider/g" videos.json
+# perl -pi -e "s/field_video_data/data/g" videos.json
+# perl -pi -e "s/field_video_duration/duration/g" videos.json
+# perl -pi -e "s/field_video_value/videoId/g" videos.json
+# perl -pi -e "s/\{/{/g" videos.json
+perl -pi -e "s/\"published\": 1/\"published\": true/g" videos.json
+
+# perl -pi -e "s/name:/\"name\":/g" videos.json
+
+# perl -pi -e "s/name:/\"name\":/g" videos.json
+# perl -pi -e "s/webURLs:/\"webURLs\":/g" videos.json
 
 
 mongo $host/$mongo_db -u $user -p $pass --eval "db.videos.drop()"
--- scripts/videos.sql
+++ scripts/videos.sql
@@ -1,7 +1,7 @@
 SELECT 
           GROUP_CONCAT(
-               CONCAT("{name:'",speaker.name,"'"),
-               CONCAT(",webURLs:'",speaker.webURLs,"'}")
+               CONCAT("{name:""",speaker.name,""),
+               CONCAT(",webURLs:""",speaker.webURLs,"""}")
           ) AS speaker, bio.field_bio_value AS  "description", 
 video.title AS  "title", 
 video_data.field_video_embed as "url", 
@@ -9,15 +9,15 @@ video_data.field_video_value as "videoId",
 video_data.field_video_provider as "provider",
 video_data.field_video_duration as "duration",
 video_data.field_video_data as "data",
-term.json as "category", true as "published"
+term.json as "old_categories", true as "published"
 FROM dr_content_field_speakerref AS speakerref
 left outer JOIN dr_node AS video ON speakerref.vid = video.vid
 left outer JOIN dr_content_field_bio AS bio ON speakerref.field_speakerref_nid = bio.nid
 left JOIN dr_content_type_video as video_data on video_data.vid = speakerref.vid
 LEFT OUTER JOIN (
-	SELECT node.nid, CONCAT(" [",
+	SELECT node.nid, CONCAT("[",
           GROUP_CONCAT(
-               CONCAT("{name:'",name,"'}")
+               CONCAT("{name:""",name,"""}")
           )
      ,"]") AS json
 	FROM dr_term_node AS node
@@ -32,8 +32,8 @@ INNER JOIN dr_node AS speaker ON speakerref.field_speakerref_nid = speaker.nid
 INNER JOIN (
 	SELECT node.nid,  CONCAT("[",
           GROUP_CONCAT(
-               CONCAT("{url:'",field_bio_website_url,"'"),
-               CONCAT(",title:'",field_bio_website_title,"'}")
+               CONCAT("{url:""",field_bio_website_url,""),
+               CONCAT(",title:""",field_bio_website_title,"""}")
           )
      ,"]") AS json
 	FROM dr_content_field_bio_website AS node
