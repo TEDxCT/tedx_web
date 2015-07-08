b83598566f9237afd4da0d0336c671b38e338b69
Carla Goldstein <carla.goldstein@gmail.com>
2015-07-08 17:33:19 +0200

    added italics styling to example blurb and keywords

--- client/views/management/talks.js
+++ client/views/management/talks.js
@@ -125,14 +125,11 @@ Template.tagTalk.events({
     categories.industries = industry;
 
     var commaSeparatedKeywords = $('#keywords').val();
-    console.log(commaSeparatedKeywords);
     keywords = commaSeparatedKeywords.split(',');
-    console.log(keywords);
 
     var blurb1 = $('#blurb1').val();
     var blurb2 = $('#blurb2').val();
     var blurb3 = $('#blurb3').val();
-    console.log(blurb1);
 
     videos.update(this._id, {$set: {categories: categories, keywords: keywords, blurb1: blurb1, blurb2:blurb2, blurb3:blurb3 }});
     Router.go("/talks/" + this._id);
