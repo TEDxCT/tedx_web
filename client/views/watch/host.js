567e7a692eb2dd8cf34f075c7f733f36cbe38759
Craig Wattrus <cwattrus@gmail.com>
2015-07-20 20:14:26 +0200

    Adding the basics for simulcast

--- client/views/watch/host.js
+++ client/views/watch/host.js
@@ -1,4 +1,47 @@
-Template.live.onCreated(function() {
+Template.host.onCreated(function() {
+  if(Session.get("draftViewingParty") == undefined) {
+    var newViewingParty = new Object();
+    newViewingParty.private = false;
+    Session.set("draftViewingParty", newViewingParty);
+  }
+});
+
+Template.host.helpers({
+  "party": function() {
+    return Session.get("draftViewingParty");
+  }
+})
+
+Template.host.events({
+  "keyup .partyName": function() {
+    setFieldOnSessionObject("draftViewingParty", "title", $(".partyName").val());
+  },
+  "keyup .entryFee": function() {
+    setFieldOnSessionObject("draftViewingParty", "entry", $(".entryFee").val());
+  },
+  "click #private": function() {
+    setFieldOnSessionObject("draftViewingParty", "private", $("#private").is(":checked"));
+  },
+  "keyup .fullName": function() {
+    setFieldOnSessionObject("draftViewingParty", "fullName", $(".fullName").val());
+  },
+  "keyup .phoneNumber": function() {
+    setFieldOnSessionObject("draftViewingParty", "phone", $(".phoneNumber").val());
+  },
+  "keyup .emailAddress": function() {
+    setFieldOnSessionObject("draftViewingParty", "email", $(".emailAddress").val());
+  }
+})
+
+function setFieldOnSessionObject(objectName, fieldName, fieldValue) {
+  var sessionObject = Session.get(objectName);
+  check(sessionObject, Object);
+  sessionObject[fieldName] = fieldValue;
+
+  Session.set(objectName, sessionObject);
+}
+
+Template.hostLocation.onCreated(function() {
   // We can use the `ready` callback to interact with the map API once the map is ready.
   GoogleMaps.ready('simulcastMap', function(map) {
     // Add a marker to the map once it's ready
@@ -9,7 +52,7 @@ Template.live.onCreated(function() {
   });
 });
 
-Template.live.helpers({
+Template.hostLocation.helpers({
   simulcastMapOptions: function() {
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
@@ -25,7 +68,7 @@ Template.live.helpers({
   }
 });
 
-Template.live.events({
+Template.hostLocation.events({
   'click .submit-search': function(event, template) {
     var searchTerm = $(template.find(".search")).val();
     console.log(searchTerm);
