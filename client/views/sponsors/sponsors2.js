c366cd74ec59d9d96d62ad529b8248f5f7134a56
Carla Goldstein <carla.goldstein@gmail.com>
2015-07-22 10:36:41 +0200

    saving images for sponsors working. Clicking on a sponsor from the home page now takes you to that sponsor's viewing page.

--- client/views/sponsors/sponsors2.js
+++ client/views/sponsors/sponsors2.js
@@ -25,19 +25,17 @@ Template.sponsors.helpers({
 
 Template.newSponsor2.events({
   'click .upload': function() {
-    console.log('Click upload image')
-    // var self = this;
     filepicker.pick({maxSize: 4*1024*1024}, function onSuccess(Blob){
       $('#imageUpload').attr("src", Blob.url);
     });
   },
   'click .save_sponsor': function(event, template) {
-    console.log('Save')
     var newSponsor = new Object();
     newSponsor.name = $(template.find(".nameText")).val();
     newSponsor.description = $(template.find(".descriptionText")).val();
     newSponsor.webURL = $(template.find(".websiteURL")).val();
     newSponsor.logoImage = $(template.find("#imageUpload")).val();
+    newSponsor.imageURL = $('#imageUpload').attr("src");
     console.log(newSponsor)
     sponsors.insert(newSponsor)
   },
