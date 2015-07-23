91843b0b20db95315cb58e2a3a76b4cad36f91ef
Carla Goldstein <carla.goldstein@gmail.com>
2015-07-23 08:31:28 +0200

    added years to sponsors.

--- client/views/sponsors/sponsors2.js
+++ client/views/sponsors/sponsors2.js
@@ -3,64 +3,61 @@ Meteor.subscribe('sponsors');
 
 Template.sponsors.rendered = function() {
   var owl = $("#owl-demo");
-  owl.owlCarousel({
-      autoPlay: 1000, //Set AutoPlay to 3 seconds
-      items : 4,
-      autoplay: true,
-      loop : true,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
-
-  });
+  if (owl != undefined) {
+    owl.owlCarousel({
+        autoPlay: 1000, //Set AutoPlay to 3 seconds
+        items : 4,
+        autoplay: true,
+        loop : true,
+        // itemsDesktop : [1199,3],
+        // itemsDesktopSmall : [979,3]
+    });
+  }
 };
 
 Template.sponsors.helpers({
   'sponsors': function() {
     var s = sponsors.find();
-    console.log(sponsors.find().fetch())
     return s;
   },
 })
 
 
 Template.newSponsor2.events({
-  'click .upload': function() {
-    filepicker.pick({maxSize: 4*1024*1024}, function onSuccess(Blob){
-      $('#imageUpload').attr("src", Blob.url);
-      $('.logoURL').attr("value", Blob.url);
-
-    });
+  'click .upload': function(event, template) {
+    uploadWithFilePicker(template)
   },
   'click .btn-upload-from-web': function(event, template) {
     event.preventDefault();
-    var imageURL = $(template.find(".logoURL")).val();
-    $('#imageUpload').attr("src", imageURL);
+    uploadFromWebURL(template);
   },
   'click .save_sponsor': function(event, template) {
     var newSponsor = new Object();
+    var years = []
+    $("input:checkbox[name=year]:checked").each(function(){
+      years.push($(this).val());
+    });
+    newSponsor.years = years;
     newSponsor.name = $(template.find(".nameText")).val();
+    newSponsor.type = $(template.find(".typeText")).val();
     newSponsor.description = $(template.find(".descriptionText")).val();
     newSponsor.webURL = $(template.find(".websiteURL")).val();
     newSponsor.logoImage = $(template.find("#imageUpload")).val();
     newSponsor.imageURL = $('#imageUpload').attr("src");
     newSponsor.type = $(template.find(".typeText")).val();
-    console.log(newSponsor)
-    sponsors.insert(newSponsor)
-    Router.go('/sponsors/show/' + this._id)
+    console.log(newSponsor);
+    var newId = sponsors.insert(newSponsor)
+    Router.go('/sponsors/show/' + newId)
   },
 });
 
 Template.editSponsor.events({
-  'click .upload': function() {
-    filepicker.pick({maxSize: 4*1024*1024}, function onSuccess(Blob){
-      $('#imageUpload').attr("src", Blob.url);
-      $('.logoURL').attr("value", Blob.url);
-    });
+  'click .upload': function(event, template) {
+    uploadWithFilePicker(template)
   },
   'click .btn-upload-from-web': function(event, template) {
     event.preventDefault();
-    var imageURL = $(template.find(".logoURL")).val();
-    $('#imageUpload').attr("src", imageURL);
+    uploadFromWebURL(template);
   },
   'click .update_sponsor': function(event, template) {
     sponsors.update({"_id": this._id}, {$set: {
@@ -71,4 +68,26 @@ Template.editSponsor.events({
     }});
     Router.go('/sponsors/show/' + this._id)
   },
+});
+
+function uploadWithFilePicker(template) {
+  filepicker.pick({maxSize: 4*1024*1024}, function onSuccess(Blob){
+    $('#imageUpload').attr("src", Blob.url);
+    $('.logoURL').attr("value", Blob.url);
+  });
+}
+
+function uploadFromWebURL(template) {
+  var imageURL = $(template.find(".logoURL")).val();
+  if (imageURL != undefined && imageURL != "") {
+    $('#imageUpload').attr("src", imageURL);
+  }
+}
+
+Template.registerHelper("isInYear", function (selectedYears, year) {
+    var index = selectedYears.indexOf(year);
+    if (index>-1) {
+      return true;
+    }
+    return false;
 });
