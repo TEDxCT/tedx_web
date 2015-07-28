ef1b8cde5ed9e6279eee6f28e50da32044b1b14c
Craig Wattrus <cwattrus@gmail.com>
2015-07-28 20:16:15 +0200

    Adding the rest of the validation and also added the correct video

--- client/views/watch/host.js
+++ client/views/watch/host.js
@@ -45,7 +45,7 @@ Template.hostDetails.events({
   },
   "click .chooseLocationLink": function() {
     if(validateForm("partyDetailsForm", {})) {
-
+      Router.go('hostlocation');
     }
   }
 })
@@ -181,7 +181,7 @@ Template.hostLocation.events({
     var viewingPartyForSaving = Session.get("draftViewingParty");
 
     var savedViewingParty;
-
+    if(validateForm("partyLocationForm")) {
       if(viewingPartyForSaving._id != undefined) {
         // saving and then removing ID off the object for saving because it would try change the documents ID
         var idForSaving = viewingPartyForSaving._id;
@@ -195,15 +195,18 @@ Template.hostLocation.events({
         Session.set("draftViewingParty", undefined);
         Router.go("hosted", {"_id": savedViewingParty});
       }
+    }
   }
 });
 
 function validateForm(formID, fieldsToExclude) {
-  console.dir($('#formID'));
+  var numberOfInvalidFields = 0;
   $('#' + formID + ' *').filter(':input').each(function(index, value){
-    // $('#' + formID + ' *').filter(':input')[index].attr("class", "error");
-    console.log(value.value.toString());
-    if(value.value.toString()=="") $('#' + formID + ' *').filter(':input')[index].attributes.class.value += " error";
+    if(value.value.toString()=="") {
+      $('#' + formID + ' *').filter(':input')[index].attributes.class.value += " error";
+      numberOfInvalidFields++;
+    }
   });
-  return false;
+  if(numberOfInvalidFields==0) return true;
+  else return false;
 }
