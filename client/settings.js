5622a992288726689cd197942905654301521dd2
Craig Wattrus <cwattrus@gmail.com>
2015-08-01 16:43:04 +0200

    Removing more font related stuff. Switched off appcache for mobile

---
+++ client/settings.js
@@ -0,0 +1,18 @@
+Meteor.AppCache.config({
+  chromeMobileIOS: false,
+  mobileSafari: false
+});
+
+Meteor.startup(function() {
+  if(Meteor.isClient){
+    loadFilePicker('A3zo8DgItQxiOLaCAjmwUz');
+
+    var options = {
+      keepHistory: 1000 * 60 * 5,
+      localSearch: true
+    };
+    var fields = ['title', 'description'];
+
+    VideosSearch = new SearchSource('videos', fields, options);
+  }
+});
