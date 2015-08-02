abfeae41596d9e9b01a17e8421321ddad3873cf3
Carla Goldstein <carla.goldstein@gmail.com>
2015-08-02 10:12:58 +0200

    cleaned up some of the file structure Hooked up adding an event.

--- client/views/events/events.js
+++ client/views/events/events.js
@@ -10,6 +10,21 @@ Template.events.helpers({
   },
 });
 
+Template.events.events({
+  'click .newEvent': function(event, template) {
+    // event.preventDefault();
+    var newPost = posts.insert({
+      "type": "event",
+      "title": "New Event",
+      "published": false,
+      "author": Meteor.userId(),
+      "created_at": $.now(),
+      "sections": [{"type": "text", "content": "Click here to edit this text"}, {"type": "image", "source": "/images/default/image.png"}]
+    });
+    Router.go('posts.edit.event', {"_id": newPost});
+  }
+})
+
 Template.eventTile.helpers({
   'summary': function() {
     if((this.summary==undefined)||(this.summary=="")) {
