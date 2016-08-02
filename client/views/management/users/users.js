a4943173a98db8a79f853824940a1db251ea7c97
Craig Wattrus <cwattrus@gmail.com>
2016-08-02 14:04:58 +0200

    Adding some checking to the user page

--- client/views/management/users/users.js
+++ client/views/management/users/users.js
@@ -8,19 +8,25 @@ Template.registerHelper('users',function(){
 Template.registerHelper('compareUserIds',function(checkboxId){
   var currentUserId = Meteor.userId();
   var admin = Meteor.users.findOne({'emails.address' : {$in: ['admin@tedxcapetown.org']}});
-  if (currentUserId == checkboxId || admin._id == checkboxId) {
-    return true;
+
+  if((Match.test(currentUserId, String)&&(Match.test(admin, Object))) {
+    if (currentUserId == checkboxId || admin._id == checkboxId) {
+      return true;
+    }
   }
+
   return false;
 });
 
 Template.registerHelper('checkRole', function(userId, roleToCheck) {
   var userWithRoles = Meteor.users.findOne({"_id" : userId}, {"roles" : 1});
   var hasRole = false;
-  if (userWithRoles.roles != undefined) {
-    var index = userWithRoles.roles.indexOf(roleToCheck);
-    if (index > -1) {
-      hasRole = true;
+  if(Match.test(userWithRoles, Object)) {
+    if (userWithRoles.roles != undefined) {
+      var index = userWithRoles.roles.indexOf(roleToCheck);
+      if (index > -1) {
+        hasRole = true;
+      }
     }
   }
   return hasRole;
