2bbfe08f36aa35ae5f6cf4603fef9cf8a5e4b152
Dane Rossenrode <info@touchdreams.co.za>
2016-09-10 11:03:54 +0200

    Updated year to a variable (all helpers), set to 2016

--- client/views/sponsors/allSponsors.js
+++ client/views/sponsors/allSponsors.js
@@ -1,18 +1,35 @@
+const currentYear = '2016';
+
 Template.allSponsors.helpers({
   'headlineSponsors': function() {
-    var s = sponsors.find({$and: [{"isHeadlineSponsor" : true}, {"published" :  {$not: false}}, {"years" : { $in: ["2015"]}}]});
+    var s = sponsors.find(
+      {$and: [
+        {"isHeadlineSponsor" : true},
+        {"published" :  {$not: false}},
+        {"years" : { $in: [currentYear]}}
+      ]});
     return s;
   },
   'sponsors': function() {
-    var s = sponsors.find({$and: [{"isHeadlineSponsor" :  {$not: true}}, {"published" :  {$not: false}}, {"years" : { $in: ["2015"]}}]}, {sort: {"priority" : 1}});
+    var s = sponsors.find(
+      {$and: [
+        {"isHeadlineSponsor" :  {$not: true}},
+        {"published" :  {$not: false}},
+        {"years" : { $in: [currentYear]}}
+      ]},
+      {sort: {"priority" : 1}});
     return s;
   },
   'draftSponsors': function() {
-    var s = sponsors.find({"published" :  false});
+    var s = sponsors.find(
+      {"published" :  false}
+    );
     return s;
   },
   'previousSponsors': function() {
-    var s = sponsors.find({"years" : { $nin: ["2015"]}});
+    var s = sponsors.find(
+      {"years" : { $nin: [currentYear]}}
+    );
     return s;
   },
 })
